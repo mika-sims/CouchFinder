a5d9ddbc045b4f726054087779e3dd610c931647
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-09 00:59:37 +0200

    Change is_active default status to True

--- accounts/models.py
+++ accounts/models.py
@@ -18,7 +18,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(default=timezone.now)
     last_login = models.DateTimeField(default=timezone.now)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
