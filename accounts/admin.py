2bd4832d79d9bd35af14b9c7e66bb3a3fff28c6a
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-10 19:42:59 +0200

    Register custom user and custom user profile to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser, CustomUserProfile
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+        'is_staff',
+    )
+
+
+@admin.register(CustomUserProfile)
+class CustomUserProfileAdmin(admin.ModelAdmin):
+    pass
