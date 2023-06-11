ba9a900e20bcc2c46099040aabc2af5a7140e9aa
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-11 16:45:38 +0200

    Create EditUserProfileView view

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,14 @@
-from django.views.generic import TemplateView
+from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from . import forms
 from . import models
 
 
-class UserProfileView(TemplateView, LoginRequiredMixin):
+class UserProfileView(generic.TemplateView, LoginRequiredMixin):
     """
     View for the user profile page.
     """
@@ -16,3 +19,17 @@ class UserProfileView(TemplateView, LoginRequiredMixin):
         context['profile'] = models.CustomUserProfile.objects.get(
             user=self.request.user)
         return context
+
+
+@method_decorator(login_required, name='dispatch')
+class EditUserProfileView(generic.UpdateView):
+    model = models.CustomUserProfile
+    form_class = forms.UpdateUserProfileForm
+    template_name = 'edit_profile.html'
+    success_url = reverse_lazy('user_profile')
+    
+    def get_object(self):
+        return models.CustomUserProfile.objects.get(user=self.request.user)
+    
+    def get_success_url(self):
+        return reverse_lazy('user_profile')
