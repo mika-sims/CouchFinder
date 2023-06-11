007319699594848f96c0b9ab9e07cc30a5fd13c6
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-11 14:00:32 +0200

    Create User Profile View to display user profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from . import forms
+from . import models
+
+
+class UserProfileView(TemplateView, LoginRequiredMixin):
+    """
+    View for the user profile page.
+    """
+    template_name = 'user_profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = models.CustomUserProfile.objects.get(
+            user=self.request.user)
+        return context
