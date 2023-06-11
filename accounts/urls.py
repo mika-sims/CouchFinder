5f01bef2505e12f1c4e715c2318e1ed657eb06b8
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-11 14:01:09 +0200

    Add UserProfileView to urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from . import views
+
 
 urlpatterns = [
-    # path('', views.home, name='home'),
+    path('user_profile/', views.UserProfileView.as_view(), name='user_profile'),
 ]
