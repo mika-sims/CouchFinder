e8915dd116388b7013e8c132fa6aaa0f1551fbe1
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-13 01:13:06 +0200

    Edit: Change view name

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-    path('user_profile/', views.UserProfileView.as_view(), name='user_profile'),
-    path('update_profile/', views.UpdateProfileView.as_view(), name='update_profile'),
+    path('user_profile/',
+         views.UserProfileView.as_view(), name='user_profile'),
+    path('update_profile/',
+         views.UpdateUserProfileView.as_view(), name='update_profile'),
 ]
