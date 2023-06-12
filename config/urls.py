12d50a1177f47ebbb819234b6999e4772e470d57
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-13 01:19:48 +0200

    Add media directory to urlpatterns for development

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from accounts import views
 
@@ -11,3 +13,7 @@ urlpatterns = [
     path('get-regions/', views.get_regions, name='get_regions'),
     path('get-cities/', views.get_cities, name='get_cities'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
