331b145c37e734958bf3eeaa656e756ce51267ff
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-12 16:09:52 +0200

    Add get_regions and get_cities to urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('accounts/', include('allauth.urls')), # django-allauth
-    path('accounts/', include('accounts.urls')), # cccounts App
+    path('accounts/', include('allauth.urls')),  # django-allauth
+    path('accounts/', include('accounts.urls')),  # cccounts App
+    path('get-regions/', views.get_regions, name='get_regions'),
+    path('get-cities/', views.get_cities, name='get_cities'),
 ]
