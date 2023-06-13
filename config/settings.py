f2f67f29590845d47501f3c1f70d41e90ad33a88
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-13 17:09:32 +0200

    Change google social providers configuration

--- config/settings.py
+++ config/settings.py
@@ -92,9 +92,8 @@ SOCIALACCOUNT_PROVIDERS = {
             'email',
         ],
         'AUTH_PARAMS': {
-            'access_type': 'offline',
+            'access_type': 'online',
         },
-        'OAUTH_PKCE_ENABLED': True,
     },
     'facebook': {
         'METHOD': 'oauth2',
