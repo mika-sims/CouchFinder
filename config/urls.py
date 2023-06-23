94bbcb84de4ff428a9a8d705f118442b253418dd
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-23 13:28:34 +0200

    Change order of the paths

--- config/urls.py
+++ config/urls.py
@@ -8,8 +8,8 @@ from accounts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('accounts/', include('accounts.urls')),  # accounts App
     path('accounts/', include('allauth.urls')),  # django-allauth
-    path('accounts/', include('accounts.urls')),  # cccounts App
     path('get-regions/', views.get_regions, name='get_regions'),
     path('get-cities/', views.get_cities, name='get_cities'),
 ]
