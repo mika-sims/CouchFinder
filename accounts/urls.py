90a215cd71d433d8d73d1fec62768d280bf5d98e
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-23 17:46:19 +0200

    Add CustomPasswordResetFromKeyView view to urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 from . import views
 
@@ -7,4 +9,6 @@ urlpatterns = [
          views.UserProfileView.as_view(), name='user_profile'),
     path('update_profile/',
          views.UpdateUserProfileView.as_view(), name='update_profile'),
+    path('password/reset/key/done/', views.CustomPasswordResetFromKeyView.as_view(),
+         name='password_reset_from_key_done'),
 ]
