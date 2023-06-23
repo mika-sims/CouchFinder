a1d277ad917500ee00f22760c635111e4d940832
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2023-06-23 13:27:34 +0200

    Change state of to nav-links on scroll or on click

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,52 @@
+// Change the current state of the navlink to active
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#navMenu');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const navMenu = document.body.querySelector('#navMenu');
+    if (navMenu) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#navMenu',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarSupportedContent .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+
 // Auto-dismiss the messages after 2 seconds
 const autoDismissTime = 2000;
 const alerts = document.querySelectorAll('.message');
